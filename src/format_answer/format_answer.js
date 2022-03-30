88f4585ecba0173eb46479eaa74b6ffda3734461
Yuri S <yszuszko123@gmail.com>
2022-03-30 14:18:31 -0300

    feat(app): added a 'formatAnswer' module so the main file doesn't cluster

--- src/format_answer/format_answer.js
+++ src/format_answer/format_answer.js
@@ -6,4 +6,4 @@ const EXPECTED_ANSWERS = ['y', 'yes', 'ye'];
 
  * * Returns a boolean(true/false)
 */
-export default formatAnswer = (answer) => EXPECTED_ANSWERS.includes(answer);
+export const formatAnswer = (answer) => EXPECTED_ANSWERS.includes(answer);
