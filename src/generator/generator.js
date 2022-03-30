eee3613b36089db0bfd972a0fc2d02f6f81140b5
Yuri S <yszuszko123@gmail.com>
2022-03-30 14:49:21 -0300

    feat(generator): made 'includeCapitalizedLetters' feature so now it generates a password with capitalized letters(wow)

--- src/generator/generator.js
+++ src/generator/generator.js
@@ -2,27 +2,38 @@ const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 const numbers = '1234567890';
 const symbols = '!@#$%&*()_';
 
-// Function that returns a number based on a maximum number;
+// * Function that returns a number based on a maximum number;
 function getRandomNumber(maximumNumber) {
     return parseInt(Math.random() * maximumNumber);
 };
 
-// Getter functions that returns random characters based
-// on the 'generatePassword' function parameters;
-function getRandomLetter() {
-    return alphabet.charAt(getRandomNumber(alphabet.length));
+/** 
+ * * Getter functions that returns random characters based
+ * * on the 'generatePassword' function parameters;
+ * 
+ * @param capitalizeLetter bool
+*/
+const getRandomLetter = (capitalizeLetter) => {
+    let randomLetter = alphabet.charAt(getRandomNumber(alphabet.length));
+    if (capitalizeLetter) {
+        let randomNumber = getRandomNumber(2);
+        if (randomNumber % 2 === 0) {
+            return randomLetter.toUpperCase();
+        };
+    };
+
+    return randomLetter;
 };
 
-function getRandomNumberCharacter() {
-    return numbers.charAt(getRandomNumber(numbers.length));
-};
+const getRandomNumberCharacter = () => numbers.charAt(getRandomNumber(numbers.length));
+const getRandomSymbol = () => symbols.charAt(getRandomNumber(symbols.length));
 
-function getRandomSymbol() {
-    return symbols.charAt(getRandomNumber(symbols.length));
-};
-
-// Function which generates a new password using the given parameters;
-function generatePassword(passwordLength, includesSymbol, includesNumber) {
+/** 
+ * * Function which generates a new password using the given parameters;
+ * @param passwordInformation array Includes all the password informations
+*/
+export default function generatePassword(passwordInformation) {
+    let [passwordLength, includesNumber, includesSymbol, includesCapitalizedLetters] = passwordInformation;
     let password = '';
 
     for (let length = 0; length < passwordLength; length++) {
@@ -33,11 +44,9 @@ function generatePassword(passwordLength, includesSymbol, includesNumber) {
         } else if ((passwordLength % randomNumber) === passwordLength && includesNumber) {
             password += getRandomNumberCharacter();
         } else {
-            password += getRandomLetter();
+            password += getRandomLetter(includesCapitalizedLetters);
         };
     };
 
     return password;
 };
-
-export { generatePassword };
